7244b964124559c915920edd1c4ed8f0a45a6eb1
Fekkes <henricus.fekkes@gmail.com>
2024-09-09 00:02:54 +0200

    - Dependency container implementation - Updated Cpp version to 20

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,47 @@
 #include <iostream>
+#include "FactoryContainer.hpp"
 
-int main(int argc, char* argv[]) 
-{ 
-    std::cout << "Hello World\n";
+class Wheel
+{
+public:
+	Wheel(FactoryContainer& fc) {}
+};
+
+class Car
+{
+public:
+	Car(FactoryContainer& fc, const std::string& brand) {
+		pWheel1 = fc.Create<Wheel>();
+		pWheel2 = fc.Create<Wheel>();
+		pWheel3 = fc.Create<Wheel>();
+		pWheel4 = fc.Create<Wheel>();
+		Brand = brand;
+	}
+
+	~Car() {
+		std::cout << "Car destroyed";
+	}
+private:
+	std::string Brand;
+	Wheel* pWheel1;
+	Wheel* pWheel2;
+	Wheel* pWheel3;
+	Wheel* pWheel4;
+};
+
+int main(int argc, char* argv[]) {
+	auto fc = FactoryContainer();
+	fc.Add<Wheel>();
+	fc.Add<Car>([](FactoryContainer& fc) {return new Car(fc, "Mercedes"); });
+	fc.Add<Car>([](FactoryContainer& fc) {return new Car(fc, "Honda"); });
+
+	auto myCar = fc.Create<Car>();
+	fc.Destroy<Car>(myCar);
+
+
+	auto c1 = &fc.Get<Car>();
+	auto c2 = &fc.Get<Car>();
+	// fc.Destroy<Car>(c1);
+
+	std::cout << "Hello World";
 }
